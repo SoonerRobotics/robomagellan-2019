ff0eab7b5fead02fa53be45fe16bd7c2ded85911
Logan Elliott <logan.a.elliott-1@ou.edu>
2018-10-01 21:42:38 -0500

    Framework for a IMU class

---
+++ localization/IMU.h
@@ -0,0 +1,13 @@
+#ifndef IMU
+#define IMU
+
+//https://github.com/adafruit/Adafruit_BNO055
+#include <Adafruit_BNO055>
+
+
+//Code for using the BN055 GPS
+class IMU {
+public:
+private:
+};
+#endif
