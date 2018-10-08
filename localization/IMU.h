db2d06ab3228242f290f9f2370f564479f795670
Logan Elliott <logan.a.elliott-1@ou.edu>
2018-10-08 11:44:24 -0500

    Untested IMU code

--- localization/IMU.h
+++ localization/IMU.h
@@ -6,8 +6,21 @@
 
 
 //Code for using the BN055 GPS
+/* This class doesn't automatically pull new data from sensor 
+   for get X,Y,Z to ensure they are synced. To read the current 
+   data into what XYZ pull from call read*/
 class IMU {
 public:
+	IMU();
+	bool connected();
+	bool calibrated();
+	float getX();
+	float getY();
+	float getZ();
+	void read() :
+    adafruit_bno055_offsets_t getOffset(); 
 private:
+	Adafruit_BNO055 bno;
+	sensors_event_t event;
 };
 #endif
