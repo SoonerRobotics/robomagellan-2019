c80948d94ead38e14667d1d062079c513d1d5181
Noah Zemlin <noah.zemlin@ou.edu>
2018-11-09 15:49:43 -0600

    Fix IMU and localization to what worked before

--- localization/IMU.cpp
+++ localization/IMU.cpp
@@ -1,14 +1,40 @@
 #include "IMU.h"
 
-IMU::IMU() {
-	bno = Adafruit_BNO055(55);
-	if (!bno.begin())
+void IMU::begin() {
+
+	if (!bno.begin(bno.OPERATION_MODE_CONFIG))
 	{
 		/* There was a problem detecting the BNO055 ... check your connections */
 		//throw new Exception("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!"); no exceptions in arduino lul
+		Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
 	}
+	calibrate();
+
+  bno.setMode(bno.OPERATION_MODE_NDOF);
+
 	//Use the external 32.768KHz crystal
 	bno.setExtCrystalUse(true);
+}
+
+void IMU::calibrate() {
+  adafruit_bno055_offsets_t configvals;
+
+	configvals.accel_offset_x = 0;
+	configvals.accel_offset_y = -1;
+	configvals.accel_offset_z = -23;
+
+	configvals.mag_offset_x = -1;
+	configvals.mag_offset_y = 2;
+	configvals.mag_offset_z = -1;
+
+	configvals.gyro_offset_x = -105;
+	configvals.gyro_offset_y = 90;
+	configvals.gyro_offset_z = -32;
+
+	configvals.accel_radius = 1000;
+	configvals.mag_radius = 680;
+
+	bno.setSensorOffsets(configvals);
 }
 
 bool IMU::connected() {
@@ -22,36 +48,32 @@ bool IMU::calibrated() {
 	uint8_t gyro = 0;
 	uint8_t accel = 0;
 	uint8_t mag = 0;
-	bno.getCalibration(&accel, &gyro, &accel, &mag);
-	if (sys != 3, gyro != 3, accel != 3, mag != 3){
-		return false;
-	}
-	return true;
+	bno.getCalibration(&sys, &gyro, &accel, &mag);
+  Serial.println("Calibs: ");
+  Serial.println(sys);
+  Serial.println(gyro);
+  Serial.println(accel);
+  Serial.println(mag);
+	return (gyro == 3 && accel == 3 && mag == 3);
 }
 
-float IMU::getX(){
+float IMU::getOrientX(){
     return event.orientation.x;
 }
 
-float IMU::getY() {
+float IMU::getOrientY() {
 	return event.orientation.y;
 }
 
-float IMU::getZ() {
+float IMU::getOrientZ() {
 	return event.orientation.z;
 }
 
-void IMU::read() {
+imu::Vector<3> IMU::getAccel() {
+  return bno.getVector(bno.VECTOR_ACCELEROMETER);
+}
+
+
+void IMU::update() {
 	bno.getEvent(&event);
-}
-
-adafruit_bno055_offsets_t IMU::getOffset() 
-{
-	adafruit_bno055_offsets_t offsets_type;
-	bno.getSensorOffsets(offsets_type);
-    return offsets_type;
-}
-
-void IMU::setOffset(const uint8_t* calibData) {
-	bno.setSensorOffsets(calibData);
 }
--- localization/LocalizationSetup.h
+++ localization/LocalizationSetup.h
@@ -1,9 +1,18 @@
 #ifndef LOCALIZATION_SETUP_H
 #define LOCALIZATION_SETUP_H
 
+GPSModule gps(3, 4);
+GPSQueue queue(&gps);
+
+IMU imu0; //apparently imu already exists
+
 void localizationSetup()
 {
-    
+    Serial.begin(9600);
+
+    imu0.begin();
+
+    queue.addPoint(35.210605, -97.443412); //a bit under Carson
 }
 
 #endif
