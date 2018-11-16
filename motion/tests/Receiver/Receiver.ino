358cbd6c485d782e09866a92fe539ca854535d68
ThaddeusMorris <thad18524@sbcglobal.net>
2018-11-16 15:06:08 -0600

    Small changes to receiver test Receiver.ino

--- motion/tests/Receiver/Receiver.ino
+++ motion/tests/Receiver/Receiver.ino
@@ -3,9 +3,15 @@
 #include "MotionSetup.h"
 #include "MotionLoop.h"
 
-const unsigned long kill = 1;
-const unsigned long reset = 2;
-const unsigned long pause = 3;
+#define s_running 1
+#define s_stopped 2
+#define s_paused  3
+
+const unsigned long m_kill = 1;
+const unsigned long m_reset = 2;
+const unsigned long m_pause = 3;
+
+int state = s_stopped;
 
 RF24 radio(9,10);
 
@@ -14,7 +20,7 @@ byte addresses[][6] = {"1Node","2Node"};
 void setup() {
 	radio.begin();
 
-	//This level might have to be increased for real use.
+//This level might have to be increased for real use.
 	radio.setPALevel(RF24_PA_LOW);
 	radio.maskIRQ(1,1,0);
 
@@ -27,6 +33,7 @@ void setup() {
 
 	motionSetup();
 	drivetrain.setPower(0.5);
+	state = s_running;
 }
 
 void loop() {
@@ -46,11 +53,22 @@ void receive() {
 		radio.write(&message, sizeof(unsigned long));
 		radio.startListening();
 
-		if(message==kill){
+		if(message==m_kill) {
 			drivetrain.disable();
-		}else if(message==reset){
+			state = s_stopped;
+		}else if(message==m_reset) {
 			drivetrain.enable();
-			drivetrain.setPower(0.3);
+			drivetrain.setPower(0.5);
+			state = s_running;
+		}else if(message==m_pause) {
+			if(state==s_running) {
+				drivetrain.disable();
+				state = s_paused;
+			}else if(state==s_paused) {
+				drivetrain.enable();
+				drivetrain.setPower(0.2);
+				state = s_running;
+			}
 		}
 	}
 }
