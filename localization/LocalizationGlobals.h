211de95b3b219556a95c78fc6324d9d3fa0154eb
Noah Zemlin <noah.zemlin@ou.edu>
2018-12-03 21:36:56 -0600

    Changed output of localization from I2C to Serial. Removed GPSQueue. Reverted GPSModule to working version.

--- localization/LocalizationGlobals.h
+++ localization/LocalizationGlobals.h
@@ -1,6 +1,6 @@
 #ifndef LOCALIZATION_GLOBALS_H
 #define LOCALIZATION_GLOBALS_H
 
-
+#define SERIAL_SEND_RATE 50 // The frequency at which updates are sent through Serial
 
 #endif
--- localization/LocalizationSetup.h
+++ localization/LocalizationSetup.h
@@ -2,17 +2,14 @@
 #define LOCALIZATION_SETUP_H
 
 GPSModule gps(3, 4);
-GPSQueue queue(&gps);
 
 IMU imu0; //apparently the name imu already exists
 
 void localizationSetup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
 
     imu0.begin();
-
-    queue.addPoint(35.210605, -97.443412); //a bit under Carson
 }
 
 #endif
