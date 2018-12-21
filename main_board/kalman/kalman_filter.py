6cd27e269cc879cd69c304c03c8ecc50fc8ec8ba
Justin Kleiber <jkleiber8@gmail.com>
2018-12-21 11:50:56 -0600

    Kalman filter modeling completed
    Need to collect data and test this filter to see what needs tuning
    
    TODO: create a driver program for this class so it can be implemented and tested

--- main_board/kalman/kalman_filter.py
+++ main_board/kalman/kalman_filter.py
@@ -1,15 +1,28 @@
 # This file manages the kalman filter used for robot localization
 
 # Import the matrix libraries and necessary modules
-from math import tan, radians
+from math import cos, sin, tan, radians, pow
 import numpy as np
 from numpy.linalg import inv
 import time
 
-# The kalman filter object that will be used to find the robot's location
+# The kalman filter class that will be used to find the robot's location
 
 
 class kalman_filter:
+
+    # State vector format
+    # 0: latitude
+    # 1: longitude
+    # 2: velocity
+    # 3: acceleration
+    # 4: heading
+    # 5: steering angle
+
+    # Class constants
+    NUM_STATE_VARS = 6
+    TOP_SPEED = 11.111
+    MAX_VOLTAGE = 13
 
     # Initialize the filter
     def __init__(self, xo, Po, L):
@@ -18,23 +31,23 @@ class kalman_filter:
 
         # Initialize the jacobians
         # Fk = Jacobian of the motion function
-        self.Fk = np.identity(14)
+        self.Fk = np.identity(self.NUM_STATE_VARS)
 
         # Hk = Jacobian of the measurement function
-        self.Hk = np.identity(14)
+        self.Hk = np.identity(self.NUM_STATE_VARS)
 
         # Set the initial conditions of the filter
-        self.x_estimate = np.zeros(14)
+        self.x_estimate = np.zeros(self.NUM_STATE_VARS)
         self.x_last = xo
-        self.Pk = np.zeros(14)
-        self.Pk_last = np.identity(14) * 0.1
+        self.Pk = np.zeros(self.NUM_STATE_VARS)
+        self.Pk_last = np.identity(self.NUM_STATE_VARS) * 0.1
 
         # Start the timing variables
         self.last_time = time.time()
 
         # Set up the covariance matrices
-        self.R = np.identity(14) * 0.5
-        self.Q = np.identity(14) * 0.6
+        self.R = np.identity(self.NUM_STATE_VARS) * 0.5
+        self.Q = np.identity(self.NUM_STATE_VARS) * 0.6
 
     # This is the motion function, normally represented by the function f(xk-1, uk, w) in our non-linear case
     # This function is called in the predict stage
@@ -43,47 +56,74 @@ class kalman_filter:
     # uk: input signal
     # w: process noise
     def motionModel(self, deltaT):
-        #Run the physics equations to estimate the current state
-        self.x_estimate[0] = self.x_last[0] + (self.x_last[3] * deltaT)
-        self.x_estimate[1] = self.x_last[1] + (self.x_last[4] * deltaT)
-        self.x_estimate[2] = self.x_last[2] + (self.x_last[5] * deltaT)
-        self.x_estimate[3] = self.x_last[3] + (self.x_last[6] * deltaT)
-        self.x_estimate[4] = self.x_last[4] + (self.x_last[7] * deltaT)
-        self.x_estimate[5:7] = self.x_last[5:7]
-        self.x_estimate[8] = self.x_last[8] + (self.x_last[3] / self.L) * tan(self.x_last[9])
-        self.x_estimate[9:13] = self.x_last[9:13]
+        # Run the physics equations to estimate the current state
+        self.x_estimate[0] = self.x_last[0] + (self.x_last[2] * cos(radians(self.x_last[4]))
+                                               * sin(radians(self.x_last[5])) * deltaT)
+        self.x_estimate[1] = self.x_last[1] + (self.x_last[2] * cos(radians(self.x_last[4]))
+                                               * cos(radians(self.x_last[5])) * deltaT)
+        self.x_estimate[2] = self.x_last[2] + (self.x_last[3] * deltaT)
+        self.x_estimate[3] = self.x_last[3]
+        self.x_estimate[4] = self.x_last[4] + (self.x_last[2] / self.L) * tan(self.x_last[5])
+        self.x_estimate[5] = self.x_last[5]
 
-        # Update the jacobian for the motion function
+        # Update the jacobian for the motion function based on last values
+        # Latitude depends on velocity, steering angle, and heading
+        self.Fk[0, 2] = cos(radians(self.x_last[4])) * sin(radians(self.x_last[5]))
+        self.Fk[0, 4] = self.x_last[2] * cos(radians(self.x_last[4])) * cos(radians(self.x_last[5]))
+        self.Fk[0, 5] = -self.x_last[2] * cos(radians(self.x_last[4])) * sin(radians(self.x_last[5]))
 
-    # This is the measurement function, normally represented by zk = xk + vk
+        # Longitude depends on the same things latitude does
+        self.Fk[1, 2] = cos(radians(self.x_last[4])) * cos(radians(self.x_last[5]))
+        self.Fk[1, 4] = -self.x_last[2] * sin(radians(self.x_last[4])) * cos(radians(self.x_last[5]))
+        self.Fk[1, 5] = -self.x_last[2] * cos(radians(self.x_last[4])) * sin(radians(self.x_last[5]))
+
+        # Heading depends on velocity and steering angle
+        self.Fk[4, 2] = tan(radians(self.x_last[5])) / self.L
+        self.Fk[4, 5] = (self.x_last[2] / self.L) * pow((1 / tan(self.x_last[5])), 2)
+
+    # This is the measurement function, normally represented by zk = h(xk) + vk
+    # We know the sensor data and estimated state, so this function will find vk
     # zk: measured values from sensors
     # xk: estimated value from motion model
     # vk: measurement noise
-    def measurementModel(self):
-        pass
+    def measurementModel(self, sensor_data):
+        # Initialize zk to zero value for sensor processing
+        zk = np.zeros(self.NUM_STATE_VARS)
 
-    # First we must predict the state of the robot based on the Ackermann steering model and prior input
+        # Process sensor data
+        zk[0] = sensor_data['GPS_lat']
+        zk[1] = sensor_data['GPS_lon']
+        zk[2] = (sensor_data['encoder_vel'] / 2) + (self.power_fn(sensor_data['motor_power']) / 2)  # consider adding GPS velocity
+        zk[3] = sensor_data['accel_x']
+        zk[4] = sensor_data['compass']
+        zk[5] = sensor_data['potentiometer']
+
+        # Subtract x_estimate from zk and return this vector (vk)
+        return (zk - self.x_estimate)
+
+    def power_fn(self, motor_power):
+        return (self.TOP_SPEED * motor_power / self.MAX_VOLTAGE)
+
+    # First we must predict the state of the robot based on the Ackermann steering model and prior state
+    # We can also consider the input signal in this stage, but we currently are not doing so
     def predict(self, deltaT):
-        # Calculate the uncertainty matrix
-        self.Pk = (self.Fk.dot(self.Pk_last)).dot(
-            np.transpose(self.Fk)) + self.Q
-
         # Perform a state estimation
         self.motionModel(deltaT)
+
+        # Calculate the uncertainty matrix based on the last state's jacobian of f(xk, uk)
+        self.Pk = (self.Fk.dot(self.Pk_last)).dot(np.transpose(self.Fk)) + self.Q
 
     # After prediction is complete, we need to update the predicted values by fusing them with real values
     # These real values are also noisy, so we use the kalman gain (Gk) to determine the tradeoff between sensor
     # and model data.
     # This will return the current state of the robot
-    def update(self, zk):
+    def update(self, sensor_data):
         # Calculate the Kalman Gain
-        self.Gk = (self.Pk.dot(np.transpose(self.Hk))).dot(
-            inv(self.Hk.dot(self.Pk).dot(np.transpose(self.Hk)) + self.R))
+        self.Gk = (self.Pk.dot(np.transpose(self.Hk))).dot(inv(self.Hk.dot(self.Pk).dot(np.transpose(self.Hk)) + self.R))
 
         # Update the state and uncertainty
-        self.x_estimate = self.x_estimate + \
-            self.Gk(zk - self.measurementModel())
-        self.Pk = (np.identity(14) - self.Gk.dot(self.Hk)).dot(self.Pk)
+        self.x_estimate = self.x_estimate + self.Gk(self.measurementModel(sensor_data))
+        self.Pk = (np.identity(self.NUM_STATE_VARS) - self.Gk.dot(self.Hk)).dot(self.Pk)
 
     # Run the filter on a new data point
     def run(self, sensor_data):
@@ -100,8 +140,8 @@ class kalman_filter:
         # Set the current data to the last data for future iterations
         self.x_last = self.x_estimate
         self.Pk_last = self.Pk
+        self.last_time = cur_time
 
         # Return the current coordinates according to the filter
-        #coords = [self.xk]
-        # return coords
-        pass
+        coords = np.array(self.x_estimate[0], self.x_estimate[1])
+        return coords
