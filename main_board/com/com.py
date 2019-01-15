774eceaad38598f367b2a667e476872e9c287e92
Colton Sandvik <sandvikcolton@gmail.com>
2019-01-14 19:54:32 -0600

    Slightly more working but still is broken <3

--- main_board/com/com.py
+++ main_board/com/com.py
@@ -1,82 +1,120 @@
 import serial
-from serial_device import serial_device
-from serial_device import serial_passthrough
+from main_board.com.serial_device import SerialDevice
+from main_board.com.serial_device import SerialPassthrough
 import time
 import multiprocessing
 
-#Main communications class
-#Author: Colton Sandvik
-#Allows the creation of serial devices and passthroughs
-#Manages serial traffic for parent program
+# Main communications class
+# Author: Colton Sandvik
+# Allows the creation of serial devices and passthroughs
+# Manages serial traffic for parent program
+# Serial Device IDs:
+# 0 - Main Board
+# 1 - Motion
 
-class serial_controller():
 
-	def __init__(self, process_rate = 50):
+class SerialController:
+
+	def __init__(self, process_rate=50):
+		self.logging.Formatter.converter = time.gmtime
+		self.logging.basicConfig(filename="/var/log/serial_communication.log", level=self.logging.INFO, format='%(asctime)s:%(message)s ')
 		self.devices = list()
 		self.passthrough = list()
 		self.process_rate = process_rate
 		self.process = None
 		self.stop = False
 
-	#Main process for looping automatically, runs at process rate defined in controller instantiation, default is 50ms
+	# Main process for looping automatically, runs at process rate defined in controller instantiation, default is 50ms
 	def loop_forever(self):
-		while not stop:
-			self.handle_serial()
-			time.sleep(process_rate/1000)
+		while not self.stop:
+			self.handle_serial(remove_from_queue=True)
+			time.sleep(self.process_rate/1000)
 
-	#Starts loop
+	# Starts loop
 	def start(self):
 		self.stop = False
-		for d in devices:
-			d.open()
+		for d in self.devices:
+			d.close()
+			s = d.open()
+			# d.ser.reset_output_buffer()
+			if not s:
+				try:
+					self.devices.remove(d)
+				except Exception as e:
+					self.logging.info(e)
+			try:
+				d.tx(d.birth_packet)
+			except Exception as e:
+				self.logging.info(e)
 		self.process = multiprocessing.Process(target=self.loop_forever)
 		self.process.start()
 
-	#Stops loop
+	# Stops loop
 	def stop(self):
 		self.stop = True
 
-	#Loops just once in cases of manual looping elsewhere
+	# Loops just once in cases of manual looping elsewhere
 	def loop(self):
-		self.handle_serial()
+		try:
+			self.handle_serial()
+		except Exception as e:
+			self.logging.info(e)
 
-	#Receives data for all devices and passthrough any setup passthroughs
-	def handle_serial(self):
-		for d in devices:
-			d.rx()
-		for p in passthrough:
-			p.passthrough()
+	# Receives data for all devices and passthrough any setup passthroughs
+	def handle_serial(self, remove_from_queue=False):
+		for d in self.devices:
+			try:
+				msg = d.rx(remove_from_queue=remove_from_queue)
+				d.parse_rx(msg)
+			except Exception as e:
+				self.logging.info(e)
 
-	#Transmit data for device; can also be done on the device itself
+		for p in self.passthrough:
+			try:
+				p.passthrough()
+			except Exception as e:
+				self.logging.info(e)
+
+	# Transmit data for device; can also be done on the device itself
 	def tx(self, device, writtable):
+		try:
 			device.tx(writtable)
+		except Exception as e:
+			self.logging.info(e)
 
-	#Adds device to serial controller and returns device
-	def add_device(self, address, baud):
-		d = serial_device(address, baud)
-		self.devices.append(d)
+	# Adds device to serial controller and returns device
+	def add_device(self, d):
+		if d.ser is not None:
+			self.devices.append(d)
 		return d
 
-	#Gets serial device based on it's address
+	# Gets serial device based on it's address
 	def get_device(self, address):
 		for d in self.devices:
-			if d.address = address:
+			if d.address == address:
 				return d
 		return None
 
-	#Removes serial device from serial controller
+	# Removes serial device from serial controller
 	def remove_device(self, device):
-		self.devices.remove(device)
+		try:
+			self.devices.remove(device)
+		except Exception as e:
+			self.logging.info(e)
 
-	#Establishes serial passthrough from one device to another
-	#Params:
-	#from_device: device that data is received from (uses the most recent serial data pull)
-	#to_device: destination of serial passthrough
-	#condition (optional): function that controls whether data is passed through (must return True or False for a given line read by from_device)
-	#Note: best works with devices of the same baud rate will replicate data or truncate data
+	# Establishes serial passthrough from one device to another
+	# Params:
+	# from_device: device that data is received from (uses the most recent serial data pull)
+	# to_device: destination of serial passthrough
+	# condition (optional): function that controls whether data is passed through (must return True or False for a given
+	# line read by from_device)
+	# Note: best works with devices of the same baud rate will replicate data or truncate data
 	# from_device baud > to_device baud --> truncates data
 	# from device baud < to device baud --> replicates data
-	def setup_passthrough(self, from_device, to_device, condition=None)
-		p = serial_passthrough(from_device, to_device, condition)
+	def setup_passthrough(self, from_device, to_device, condition=None):
+		try:
+			p = SerialPassthrough(from_device, to_device, condition)
+		except Exception as e:
+			self.logging.info(e)
 		self.passthrough.append(p)
 		return p
--- main_board/com/serial_device.py
+++ main_board/com/serial_device.py
@@ -1,91 +1,110 @@
-import sys
 import serial
 import json
 
-#Serial Device and Passthrough Classes
-#Author: Colton Sandvik
+# Serial Device and Passthrough Classes
+# Author: Colton Sandvik
 
-class serial_device():
+
+class SerialDevice:
 
 	def __init__(self, address, baud):
 		self.address = address
 		self.baud = baud
 		self.read_queue = list()
 		self.status = "Closed"
-		self.ser = serial.Serial(
-			port=address,
-        	baudrate = baud,
-        	parity=serial.PARITY_NONE,
-        	stopbits=serial.STOPBITS_ONE,
-        	bytesize=serial.EIGHTBITS,
-        	timeout=1
-        )
+		self.id = 0
+		self.birth_packet = """
+		{
+			device_id: 0,
+			name: "Main board",
+			event: "birth",
+			data: []
+		}
+		"""
+		try:
+			self.ser = serial.Serial(
+				port=self.address,
+				baudrate=baud,
+				parity=serial.PARITY_NONE,
+				stopbits=serial.STOPBITS_ONE,
+				bytesize=serial.EIGHTBITS,
+				timeout=1,
+				write_timeout=10
+			)
+		except Exception as e:
+			raise e
+			self.ser = None
 
 	def open(self):
 		try:
-			self.open()
+			self.ser.open()
 			self.status = "Opened"
+			self.tx(self.birth_packet)
 			return True
 		except Exception as e:
-			print(e)
+			raise e
 			return False
 
 	def close(self):
 		try:
-			self.close()
+			self.ser.close()
 			self.status = "Closed"
 			return True
 		except Exception as e:
-			print(e)
+			raise e
 			return False
 
-	#Reads line already pulled from device and stored in queue
+	# Reads line already pulled from device and stored in queue
 	def readline(self, release=True):
 		if len(self.read_queue) != 0:
 			t = self.read_queue[0]
 			if release:
 				self.finish_read()
-			return self.read_queue[0]
+			return t
 		else:
 			return ""
 
-	#Releases data from queue, by default is done after readline
+	# Releases data from queue, by default is done after readline
 	def finish_read(self):
 		if len(self.read_queue) != 0:
 			del self.read_queue[0]
 
-	#Receives data and returns if queue is to be skipped
+	# Receives data and returns if queue is to be skipped
 	def rx(self, remove_from_queue=False):
 		try:
-			l = self.ser.readline(release=False)
+			l = self.ser.readline()
 			if not remove_from_queue:
 				self.read_queue.append(l)
 			else:
 				return l
 		except serial.SerialException as e:
-			print(e)
+			raise e
 
-	#Transmits data down device pipeline can take any object, no conversions necessary
+	# Transmits data down device pipeline can take any object, no conversions necessary
 	def tx(self, writtable):
 		if isinstance(writtable, dict):
 			try:
 				self.ser.write(json.dumps(writtable) + '\n')
 			except serial.SerialException as e:
-				print(e)
+				raise e
 		elif isinstance(writtable, str):
 			try:
-				self.ser.write(writtable)
+				self.ser.write(writtable.encode('ascii'))
+				print("Transmitting to %s: %s" % (self.address, writtable))
 			except serial.SerialException as e:
-				print(e)
+				raise e
 		else:
 			try:
-				self.ser.write(str(writtable) + '\n')
+				self.ser.write(bytes(str(writtable) + '\n', 'ascii'))
 			except serial.SerialException as e:
-				print(e)
+				raise e
+
+	def parse_rx(self, msg):
+		pass
 
 
-#Serial Passthrough class
-class serial_passthrough():
+# Serial Passthrough class
+class SerialPassthrough:
 
 	def __init__(self, from_device, to_device, condition=None):
 		self.from_device = from_device
@@ -95,10 +114,50 @@ class serial_passthrough():
 		else:
 			self.condition = lambda feed : True
 
-	#Passes data through without releasing it
-	#If condition is set it will only send data to destination if condition is true
+	# Passes data through without releasing it
+	# If condition is set it will only send data to destination if condition is true
 	def passthrough(self):
-		feed = self.from_device.readline(release=False)
+		try:
+			feed = self.from_device.readline(release=False)
+		except Exception as e:
+			raise e
 		if self.condition(feed):
 			self.to_device.tx(feed)
 		return feed
+
+# Creating Communication Endpoint
+# Create class that extends the serial_device base class
+# Override the parse_rx method to convert the stringified JSON from the device into the standardized JSON command
+# structure exampled below:
+# {
+#	device_id: 0,
+#	name: "Motion",
+#	event: "motion_collect",
+#	data: {
+#		x: 20,
+#		accel: 0.6
+#	}
+# }
+# This may eventually be automated but is good enough for now.
+
+# WIP Motion Endpoint
+
+
+class MotionSerial(SerialDevice):
+
+	def __init__(self, address, baud):
+		SerialDevice.__init__(self, address, baud)
+		self.id = 1
+
+	def parse_rx(self, msg):
+		j = json.loads(msg)
+
+
+class LocalizationSerial(SerialDevice):
+
+	def __init__(self, address, baud):
+		SerialDevice.__init__(self, address, baud)
+		self.id = 2
+
+	def parse_rx(self, msg):
+		j = json.loads(msg)
