5b2dc8f8761cfdbddbf8dabc308318f6f62b3aba
jkleiber <jkleiber8@gmail.com>
2019-02-06 20:47:05 -0600

    Adds EKF process piping to the main process and merges changes Sets up config stuff

--- main_board/com/com.py
+++ main_board/com/com.py
@@ -1,5 +1,5 @@
 import serial
-from .serial_device import *
+from com.serial_device import *
 import time
 import multiprocessing
 from multiprocessing import Queue
@@ -19,125 +19,143 @@ import logging
 
 class SerialController:
 
-    def __init__(self, daddy_pipe, process_rate=50):
-        self.daddy_pipe = daddy_pipe
-        logging.Formatter.converter = time.gmtime
-        logging.basicConfig(filename="/var/log/serial_communication.log", level=logging.INFO,
-                                 format='%(asctime)s:%(message)s ')
-        self.devices = list()
-        self.birth_devices = list()
-        self.passthrough = list()
-        self.process_rate = process_rate
-        self.process = None
-        self.stop = False
-        self.state = 0
-        self.buffer = Queue()
+	def __init__(self, daddy_pipe, process_rate=50):
+		self.daddy_pipe = daddy_pipe
+		logging.Formatter.converter = time.gmtime
+		logging.basicConfig(filename="/var/log/serial_communication.log", level=logging.INFO,
+								 format='%(asctime)s:%(message)s ')
+		self.devices = list()
+		self.birth_devices = list()
+		self.passthrough = list()
+		self.process_rate = process_rate
+		self.process = None
+		self.stop = False
+		self.state = 0
+		self.buffer = Queue()
 
-    # Main process for looping automatically, runs at process rate defined in controller instantiation, default is 50ms
-    def loop_forever(self):
-        while not self.stop:
-            if self.daddy_pipe.poll():
-                event = self.daddy_pipe.recv()
-                if event['event'] == 'state':
-                    self.state = event['data']['state']
-            if self.state == 0 or len(self.birth_devices) == 0:
-                for device in self.birth_devices:
-                    if device.check_response():
-                        new_device = self.fetch_device(device, device.new_id)
-                        self.birth_devices.remove(device)
-                        # self.daddy_pipe.send(new_device)
+	# Main process for looping automatically, runs at process rate defined in controller instantiation, default is 50ms
+	def loop_forever(self):
+		while not self.stop:
+			if self.daddy_pipe.poll():
+				event = self.daddy_pipe.recv()
+				if event['event'] == 'state':
+					self.state = event['data']['state']
+				elif event['event'] == 'kalman_init':
+					self.ekf_pipe = event['data']
+				elif event['event'] == 'mapping_init':
+					self.mapping_pipe = event['data']
 
-            elif self.state == 1:
-                self.handle_serial(remove_from_queue=True)
-                time.sleep(self.process_rate / 1000)
+			# Birth packets
+			if self.state == 0 or len(self.birth_devices) == 0:
+				for device in self.birth_devices:
+					if device.check_response():
+						new_device = self.fetch_device(device, device.new_id)
+						self.birth_devices.remove(device)
+						# self.daddy_pipe.send(new_device)
 
-    # Determine device to create based off id
-    def fetch_device(self, d, did):
-        if did == 1:
-            new_d = MotionSerial(d.address, d.baud, s=d.ser)
-        elif did == 2:
-            new_d = LocalizationSerial(d.address, d.baud, s=d.ser)
-        self.devices.append(new_d)
-        return new_d
+			# Regular operations
+			elif self.state == 1:
+				# Read serial data
+				self.handle_serial(remove_from_queue=True)
 
-    # Starts loop
-    def start(self, addresses):
-        self.stop = False
-        for a in addresses:
-            self.birth_devices.append(SerialBirther(a, 115200))
-        self.process = multiprocessing.Process(target=self.loop_forever)
-        self.process.start()
+				# TODO: Parse data
 
-    # Stops loop
-    def stop(self):
-        self.stop = True
+				# Only send data to the EKF if there is a pipe set up for it
+				if self.ekf_pipe:
+					pass
 
-    # Loops just once in cases of manual looping elsewhere
-    def loop(self):
-        try:
-            self.handle_serial()
-        except Exception as e:
-            self.logging.info(e)
+				# Get most recent mapping data
 
-    # Receives data for all devices and pass through any setup passthroughs
-    def handle_serial(self, remove_from_queue=False):
-        for d in self.devices:
-            try:
-                d.execute_commands()
-                msg = d.rx(remove_from_queue=remove_from_queue)
-                d.parse_rx(msg)
-            except Exception as e:
-                self.logging.info(e)
+				# The decision (https://www.youtube.com/watch?v=2_LUnTQHV4c)
+				# Three possibilities
+				# 1. Everything is normal, just read the trajectory data to make the robot move
+				# 2. The robot is near a cone, so turn on the camera and make it hit the cone
+				# 3. The robot is going to hit an obstacle, so don't let that happen
 
-        for p in self.passthrough:
-            try:
-                p.passthrough()
-            except Exception as e:
-                self.logging.info(e)
 
-    # Transmit data for device; can also be done on the device itself
-    def tx(self, device, writtable):
-        try:
-            device.tx(writtable)
-        except Exception as e:
-            self.logging.info(e)
+				time.sleep(self.process_rate / 1000)
 
-    # Adds device to serial controller and returns device
-    def add_device(self, d):
-        if d.ser is not None:
-            self.devices.append(d)
-        return d
+	# Determine device to create based off id
+	def fetch_device(self, d, did):
+		if did == 1:
+			new_d = MotionSerial(d.address, d.baud, s=d.ser)
+		elif did == 2:
+			new_d = LocalizationSerial(d.address, d.baud, s=d.ser)
+		self.devices.append(new_d)
+		return new_d
 
-    # Gets serial device based on it's address
-    def get_device(self, address):
-        for d in self.devices:
-            if d.address == address:
-                return d
-        return None
+	# Starts loop
+	def start(self, addresses):
+		self.stop = False
+		for a in addresses:
+			self.birth_devices.append(SerialBirther(a, 115200))
+		self.process = multiprocessing.Process(target=self.loop_forever)
+		self.process.start()
 
-    # Removes serial device from serial controller
-    def remove_device(self, device):
-        try:
-            self.devices.remove(device)
-        except Exception as e:
-            self.logging.info(e)
+	# Loops just once in cases of manual looping elsewhere
+	def loop(self):
+		try:
+			self.handle_serial()
+		except Exception as e:
+			logging.info(e)
 
-    # Establishes serial passthrough from one device to another
-    # Params:
-    # from_device: device that data is received from (uses the most recent serial data pull)
-    # to_device: destination of serial passthrough
-    # condition (optional): function that controls whether data is passed through (must return True or False for a given
-    # line read by from_device)
-    # Note: best works with devices of the same baud rate will replicate data or truncate data
-    # from_device baud > to_device baud --> truncates data
-    # from device baud < to device baud --> replicates data
-    def setup_passthrough(self, from_device, to_device, condition=None):
-        try:
-            p = SerialPassthrough(from_device, to_device, condition)
-        except Exception as e:
-            self.logging.info(e)
-        self.passthrough.append(p)
-        return p
+	# Receives data for all devices and pass through any setup passthroughs
+	def handle_serial(self, remove_from_queue=False):
+		for d in self.devices:
+			try:
+				d.execute_commands()
+				msg = d.rx(remove_from_queue=remove_from_queue)
+				d.parse_rx(msg)
+			except Exception as e:
+				logging.info(e)
 
-    def get_pipe(self):
-        return self.child_pipe
+		for p in self.passthrough:
+			try:
+				p.passthrough()
+			except Exception as e:
+				logging.info(e)
+
+	# Transmit data for device; can also be done on the device itself
+	def tx(self, device, writtable):
+		try:
+			device.tx(writtable)
+		except Exception as e:
+			logging.info(e)
+
+	# Adds device to serial controller and returns device
+	def add_device(self, d):
+		if d.ser is not None:
+			self.devices.append(d)
+		return d
+
+	# Gets serial device based on it's address
+	def get_device(self, address):
+		for d in self.devices:
+			if d.address == address:
+				return d
+		return None
+
+	# Removes serial device from serial controller
+	def remove_device(self, device):
+		try:
+			self.devices.remove(device)
+		except Exception as e:
+			logging.info(e)
+
+	# Establishes serial passthrough from one device to another
+	# Params:
+	# from_device: device that data is received from (uses the most recent serial data pull)
+	# to_device: destination of serial passthrough
+	# condition (optional): function that controls whether data is passed through (must return True or False for a given
+	# line read by from_device)
+	# Note: best works with devices of the same baud rate will replicate data or truncate data
+	# from_device baud > to_device baud --> truncates data
+	# from device baud < to device baud --> replicates data
+	def setup_passthrough(self, from_device, to_device, condition=None):
+		try:
+			p = SerialPassthrough(from_device, to_device, condition)
+		except Exception as e:
+			logging.info(e)
+		self.passthrough.append(p)
+		return p
+
--- main_board/main.py
+++ main_board/main.py
@@ -1,16 +1,56 @@
 from com.serial_device import *
 from com.com import SerialController
 
+import configparser
+import numpy as np
 import time
 from lidar.mapper import Mapper
 import multiprocessing
 
+# EKF
+from kalman.kalman_filter import kalman_filter
+
 if __name__ == '__main__':
+    # Open the pipes for the main process
 	daddy_pipe, com_pipe = multiprocessing.Pipe()
 	controller = SerialController(daddy_pipe)
+    
+    # Get the robot configuration data
+	config = configparser.ConfigParser()
+	config.read('config.ini')
+
 	# Addresses to check, will probably automate this list at some point
 	addresses = ["/dev/ttyUSB1", "/dev/ttyUSB2"]
 	controller.start(addresses)
 
 	lying_camera = Mapper()
 	lying_camera.start()
+
+	# Make an initial state
+	xo = np.array(config['EKF']['start_lat'], config['EKF']['start_lon'], 0, 0, 0, 0)
+	Po = np.eye(6) * 0.2    # TODO: choose an actual initial uncertainty
+
+	# Set up the kalman filter
+	EKF = kalman_filter(xo, Po, config)
+
+	# Set up EKF piping system
+	main_pipe, ekf_pipe = multiprocessing.Pipe(True)
+
+	# Start the EKF process
+	proc = multiprocessing.Process(target = EKF.process_data, args = (ekf_pipe,))
+	proc.start()
+
+	# Set the pipe in the main process to be the main pipe declared above
+	EKF_dict = {'event':'kalman_init', 'data': main_pipe}
+	com_pipe.send(EKF_dict)
+
+	# Set the pipe for the mapper to the main process
+	# MAP_dict = {'event':'mapping_init', 'data': lying_camera.getPipe()}
+
+	# Run the separate process as long as the first element in the queue is not 'exit'
+	while True:
+	    # Send data to the EKF process
+	    # NOTE: data should be a dict of sensor data
+	    pass
+
+	proc.join()
