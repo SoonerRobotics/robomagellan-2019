307db37fe530da5be8bd2e34f5af219bbaa54f4f
Noah Zemlin <noah.zemlin@ou.edu>
2019-03-11 20:44:06 -0500

    Obstacle node

---
+++ roma_ws/src/roma_vision/src/obstacle_avoidance.cpp
@@ -0,0 +1,22 @@
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+#include "roma_ws/Obstacle.h"
+
+ros::Subscriber lidar;
+
+// TODO: Change to correct LIDAR message type
+void onLidarCallback(const std_msgs::String::ConstPtr& msg) {
+    //TODO: On LIDAR read, do things and then send obstacles on obstacle topic
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "obstacle_avoidance");
+
+    ros::NodeHandle node;
+
+    ros::Publisher obstacles = node.advertise<>("roma_vision/obstacles", 1000);
+
+    lidar = node.subscribe(node.resolveName("roma_vision/lidar"), 1000, onLidarCallback);
+
+    ros::spin();
+}
