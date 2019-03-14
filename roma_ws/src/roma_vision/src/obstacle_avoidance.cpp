df4552c0ed501669249bc20147021ff2be74f2fb
Noah Zemlin <noah.zemlin@ou.edu>
2019-03-13 20:21:29 -0500

    Basic obstacle detection algorithm

--- roma_ws/src/roma_vision/src/obstacle_avoidance.cpp
+++ roma_ws/src/roma_vision/src/obstacle_avoidance.cpp
@@ -2,11 +2,45 @@
 #include "std_msgs/String.h"
 #include "roma_ws/obstacles.h"
 
-ros::Subscriber lidar;
+ros::Publisher obstacle_topic;
+
+//TODO: What is the max distance we reasonably read at? Units?
+#define MAX_DISTANCE = 8;
+
+struct {
+    float angle,
+    float distance
+} Obstacle
 
 // TODO: Change to correct LIDAR message type
 void onLidarCallback(const std_msgs::String::ConstPtr& msg) {
     //TODO: On LIDAR read, do things and then send obstacles on obstacle topic
+
+    //TODO: Assumes array of 360 distances for each degree, change to what it actually be
+    float[] distances;
+
+    //TODO: This literally just creates an obstacle for every distance < MAX_DISTANCE. Create better algorithm.
+    std::vector<Obtsacle> obs;
+    for (int i=0; i<360; i++) {
+        if (distances[i] < MAX_DISTANCE) {
+            ob = Obstacle();
+            ob.angle = i;
+            ob.distance = distances[i]
+            obs.push_back(ob)
+        }
+    }
+
+    
+    uint8_t numObstacles = obs.size();
+    float angles[numObstacles];
+    float distances[numObstacles];
+
+    for (int i=0; i<numObstacles; i++) {
+        angles[i] = obs.get(i).angle;
+        distances[i] = obs.get(i).angle;
+    }
+
+    //TODO: Put data into message and send it.
 }
 
 int main(int argc, char** argv) {
@@ -14,9 +48,9 @@ int main(int argc, char** argv) {
 
     ros::NodeHandle node;
 
-    ros::Publisher obstacles = node.advertise<roma_vision::obstacles>("roma_vision/obstacles", 1000);
+    obstacle_topic = node.advertise<roma_vision::obstacles>("roma_vision/obstacles", 1000);
 
-    lidar = node.subscribe(node.resolveName("roma_vision/lidar"), 1000, onLidarCallback);
+    ros::Subscriber lidar = node.subscribe(node.resolveName("roma_vision/lidar"), 1000, onLidarCallback);
 
     ros::spin();
 }
