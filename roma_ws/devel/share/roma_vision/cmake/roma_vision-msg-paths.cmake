91fe2102c5a55a7a5edee028ef87300f0c4c325f
Trey Sullivent <trey.sullivent@gmail.com>
2019-03-25 20:50:27 -0500

    Removed rosserial in favor of new serial package. Backtracked back to rosserial and began modifying arduino localization code to support ROS. Removed all direct references to serial and JSON in the localization file and added several TODOs.

---
+++ roma_ws/devel/share/roma_vision/cmake/roma_vision-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(roma_vision_MSG_INCLUDE_DIRS "/home/trey/scr/robomagellan-2019/roma_ws/src/roma_vision/msg")
+set(roma_vision_MSG_DEPENDENCIES std_msgs)
---
+++ roma_ws/devel/share/rosserial_msgs/cmake/rosserial_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(rosserial_msgs_MESSAGE_FILES "/home/trey/scr/robomagellan-2019/roma_ws/src/rosserial-jade-devel/rosserial_msgs/msg/Log.msg;/home/trey/scr/robomagellan-2019/roma_ws/src/rosserial-jade-devel/rosserial_msgs/msg/TopicInfo.msg")
+set(rosserial_msgs_SERVICE_FILES "/home/trey/scr/robomagellan-2019/roma_ws/src/rosserial-jade-devel/rosserial_msgs/srv/RequestMessageInfo.srv;/home/trey/scr/robomagellan-2019/roma_ws/src/rosserial-jade-devel/rosserial_msgs/srv/RequestParam.srv;/home/trey/scr/robomagellan-2019/roma_ws/src/rosserial-jade-devel/rosserial_msgs/srv/RequestServiceInfo.srv")
