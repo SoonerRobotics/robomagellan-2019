935b6ca9145a9aa01dd37f4a00eb8c854ba9d679
Justin Kleiber <jkleiber8@gmail.com>
2019-04-10 21:09:45 -0500

    Increases buffer size on localization, now that works

--- localization/localization.ino
+++ localization/localization.ino
@@ -1,13 +1,15 @@
 #include <ros.h>
 #include "roma_msgs/localization_data.h"
 
+//FIXME: Make these things not broken. Also, use the SCR_bno055 file?
 #include "GPSModule.h"
 #include "IMU.h"
+
 #include "LocalizationGlobals.h"
 #include "LocalizationSetup.h"
 
 //Set up ROS node
-ros::NodeHandle_<ArduinoHardware, 2, 2, 32, 105> localization_node;
+ros::NodeHandle_<ArduinoHardware, 1, 1, 16, 128> localization_node;
 
 //Set up ROS publisher
 roma_msgs::localization_data local_data;
@@ -15,43 +17,39 @@ ros::Publisher data_pub("/roma_localization/data", &local_data);
  
 void setup() 
 {
+	//Set serial communication to be 115200 baud
+	Serial.begin(57600);
+
 	//Set up ROS node
 	localization_node.initNode();
 	localization_node.advertise(data_pub);
 
-    localizationSetup();
+    //localizationSetup();
 
-    intellectualWait(1000);
+	//FIXME: broken when GPS and IMU are bad
+    //intellectualWait(1000);
 
-    imu0.zero();
+	//FIXME: Unbreak the IMU
+    //imu0.zero();
 }
 
+/*
 void intellectualWait(unsigned long ms) {
     unsigned long startTime = millis();
     while (millis() - startTime < ms) {
         gps.update();
         imu0.update();
     }
-}
+}*/
 
 void loop() 
 {
-    /*
-	dataArray["gps_lat"] = gps.getLat();
-    dataArray["gps_lon"] = gps.getLong();
-    dataArray["imu_heading"] = imu0.getOrientX();
-    dataArray["imu_accel_x"] = imu0.getAccelX();
-    dataArray["imu_accel_y"] = imu0.getAccelY();
-    dataArray["encoder_dx"] = 0;
-    dataArray["encoder_dt"] = 0;
-	*/
-
     //Form localization msg
-	//local_data.gps_lat = gps.getLat();
-	//local_data.gps_lon = gps.getLong();
+	local_data.gps_lat = 0; //gps.getLat();
+	local_data.gps_lon = 0; //gps.getLong();
 	local_data.vel = 0;
-	//local_data.accel_x = imu0.getAccelX();
-	//local_data.heading = imu0.getOrientX();
+	local_data.accel_x = 0; //imu0.getAccelX();
+	local_data.heading = 0; //imu0.getOrientX();
 
 	//Publish message to topic
 	data_pub.publish(&local_data);
@@ -59,5 +57,11 @@ void loop()
 	//Spin for callbacks
 	localization_node.spinOnce();
 
-    intellectualWait(1000 / SERIAL_SEND_RATE);
+	Serial.println("yeet");
+
+	delay(100);
+	//delay(1000 / SERIAL_SEND_RATE);
+
+	//FIXME: When the GPS and IMU are not working, this needs to fail safely
+    //intellectualWait(1000 / SERIAL_SEND_RATE);
 }
