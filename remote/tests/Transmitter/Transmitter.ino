22b90c4ef257bc190a0390f857ff5f11392b0ce8
ThaddeusMorris <thad18524@sbcglobal.net>
2019-04-24 19:25:11 -0500

    Cleanup Transmitter.ino code and set nRF24 power level to high

--- remote/tests/Transmitter/Transmitter.ino
+++ remote/tests/Transmitter/Transmitter.ino
@@ -1,108 +1,93 @@
-#include <SPI.h>
 #include "RF24.h"
 
-const int kill_button = 2;
-const int reset_button = 3;
-const int pause_button = 4;
-const int success_led = 5;
-const int fail_led = 6;
+const int KILL_BUTTON = 2;
+const int RESET_BUTTON = 3;
+const int PAUSE_BUTTON = 4;
+const int SUCCESS_LED = 5;
+const int FAIL_LED = 6;
 
-const unsigned long kill = 1;
-const unsigned long reset = 2;
-const unsigned long pause = 3;
+const unsigned long KILL = 1;
+const unsigned long RESET = 2;
+const unsigned long PAUSE = 3;
 
 RF24 radio(9,10);
 
 byte addresses[][6] = {"1Node","2Node"};
 
-void setup(){
-	//When connected to a serial monitor the transmission will fail often; when disconnected it rarely fails.
-	Serial.begin(115200);
-	Serial.println(F("Starting Transmitter..."));
+void setup() {
+	// Setup pins
+	pinMode(KILL_BUTTON, INPUT);
+	pinMode(RESET_BUTTON, INPUT);
+	pinMode(PAUSE_BUTTON, INPUT);
+	pinMode(SUCCESS_LED, OUTPUT);
+	pinMode(FAIL_LED, OUTPUT);
+	digitalWrite(SUCCESS_LED, LOW);
+	digitalWrite(FAIL_LED, LOW);
 
-	pinMode(kill_button, INPUT);
-	pinMode(success_led, OUTPUT);
-	digitalWrite(success_led, LOW);
-	pinMode(fail_led, OUTPUT);
-	digitalWrite(fail_led, LOW);
-
+	// Setup radio
 	radio.begin();
-
-	//This level might have to be increased for real use.
-	radio.setPALevel(RF24_PA_LOW);
-
+	radio.setPALevel(RF24_PA_HIGH);
 	radio.openWritingPipe(addresses[1]);
 	radio.openReadingPipe(1,addresses[0]);
-
 	radio.startListening();
 }
 
-int send_message(unsigned long message){
-	Serial.print(F("Sending "));
-	Serial.println(message);
-
+bool send_message(unsigned long message) {
 	radio.stopListening();
 
-	if(!radio.write(&message,sizeof(unsigned long))){
-		Serial.println(F("Failed, unable to write"));
-		return(0);
+	if (!radio.write(&message,sizeof(unsigned long))) {
+		return 0; // Unable to write
 	}
 
-	//Listen for confirmation message
+	// Listen for confirmation message
 	radio.startListening();
 
 	unsigned long wait_start = micros();
-	boolean timeout = false;
 
-	//Don't wait forever
-	while(!radio.available()){
-		if(micros() - wait_start > 1000000){
-			timeout = true;
-			break;
+	// Don't wait forever
+	while (!radio.available()) {
+		if (micros() - wait_start > 1000000) {
+			return 0; // Timeout
 		}
 	}
 
-	if(timeout){
-		Serial.println(F("Failed, response timed out"));
-	}else{
-		unsigned long response;
-		radio.read(&response, sizeof(unsigned long));
+	unsigned long response;
+	radio.read(&response, sizeof(unsigned long));
 
-		//Check response
-		if(response == message){
-			Serial.println(F("Transmission successful"));
-			return(1); //success
-		}else{
-			Serial.println(F("Transmission corrupted"));
-		}
-	}
-	return(0); //fail
-}
-
-unsigned long read_buttons(){
-	if(digitalRead(kill_button)){
-		return(kill);
-	}else if(digitalRead(reset_button)){
-		return(reset);
-	}else if(digitalRead(pause_button)){
-		return(pause);
-	}else{
-		return(0);
+	//Check response
+	if (response == message) {
+		return 1; // Success
+	} else {
+		return 0; // Corruption
 	}
 }
 
-void loop(){
-	unsigned long button;
-	if(button = read_buttons()){
-		if(send_message(button)){
-			digitalWrite(success_led, HIGH);
+unsigned long read_buttons() {
+	if (digitalRead(KILL_BUTTON)) {
+		return KILL;
+	} else if (digitalRead(RESET_BUTTON)) {
+		return RESET;
+	} else if (digitalRead(PAUSE_BUTTON)) {
+		return PAUSE;
+	} else {
+		return 0;
+	}
+}
+
+void loop() {
+	unsigned long message;
+	if (message = read_buttons()) {
+		if (send_message(message)) {
+			// Success
+			digitalWrite(SUCCESS_LED, HIGH);
 			delay(1000);
-			digitalWrite(success_led, LOW);
-		}else{
-			for(int i=0;i<5;i++){
-				digitalWrite(fail_led, HIGH);
+			digitalWrite(SUCCESS_LED, LOW);
+		} else {
+			// Fail
+			for (int i=0;i<5;i++) {
+				digitalWrite(FAIL_LED, HIGH);
 				delay(100);
-				digitalWrite(fail_led, LOW);
+				digitalWrite(FAIL_LED, LOW);
 				delay(100);
 			}
 		}
